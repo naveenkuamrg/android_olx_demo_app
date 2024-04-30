1d27c49c4fd601bcb71e67a6fbe1d81745b0de86
naveenkumarg <naveenkumarg664@gmail.com>
2024-04-30 12:15:11 +0530

    Ui Update

--- java/com/application/compose/SortBottomSheet.kt
+++ java/com/application/compose/SortBottomSheet.kt
@@ -1,22 +1,45 @@
 package com.application.compose
 
+import android.graphics.Bitmap
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.application.R
 
 
 @Composable
-fun Check(name: String){
-   Text(text = name)
+fun Check(content: String) {
+    Column(
+        Modifier
+            .padding(10.dp)
+            .focusable(true)) {
+       Row(Modifier.fillMaxWidth()) {
+           Text(text = content)
+       }
+    }
 }
 
 @Preview()
 @Composable
-fun CheckPreview(){
-   MaterialTheme{
-      Check(name = "naveen")
-   }
+fun CheckPreview() {
+    MaterialTheme {
+//        Check()
+    }
 }
